720c26c116e2575a719e5e7978b9fc1989457d05
GopiCharanReddy <gopicharanvgcr@gmail.com>
2025-07-28 23:11:23 +0530

    Ui library button

---
+++ frontend/src/components/Button.tsx
@@ -0,0 +1,32 @@
+import type { ReactElement } from "react"
+
+type ButtonProps = {
+  variant: "primary" | "secondary",
+  size: "sm" | "md" | "lg",
+  text: string,
+  startIcon?: ReactElement,
+  endIcon?: ReactElement,
+  onClick?: () => void
+}
+
+const variantStyles = {
+  "primary": "bg-slate-950 p-4 rounded-xl",
+  "secondary": "bg-purple-800 text-slate-950 font-semibold p-4 rounded-xl"
+}
+const sizeStyles = {
+  "sm": "p-2 py-3",
+  "md": "py-3 px-4",
+  "lg": "px-4 py-4"
+}
+
+export const Button = (props: ButtonProps) => {
+  return (
+    <>
+      <button className={`flex ${variantStyles[props.variant]} cursor-pointer m-2 ${sizeStyles[props.size]}`}  >
+        {props.startIcon ? <div className="pr-1">{props.startIcon}</div> : null}
+        {props.text}
+        {props.endIcon ? <div className="pl-1">{props.endIcon}</div>: null}
+      </button>
+    </>
+  )
+}
