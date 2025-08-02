08e56bcb52096c91f4f8eb33ff621e21da07250e
GopiCharanReddy <gopicharanvgcr@gmail.com>
2025-08-03 00:05:28 +0530

    Add Header

---
+++ frontend/src/components/header/Header.tsx
@@ -0,0 +1,16 @@
+import { Button } from '../Button'
+import { PlusIcon } from '../../icons/PlusIcon'
+import { ShareIcon } from '../../icons/ShareIcon'
+const Header = () => {
+  return (
+      <div className="flex justify-between w-300 px-5 p-2 items-center">
+        <h1 className='text-black font-semibold text-3xl'>All Notes</h1>
+        <div className='flex'>
+        <Button variant='primary' size='lg' text='Share Content' startIcon={<ShareIcon size='lg' />} onClick={() => { "" }} />
+        <Button variant='secondary' size='lg' text='Add Content' startIcon={<PlusIcon size='lg' />} onClick={() => { "" }} />
+        </div>
+      </div>
+  )
+}
+
+export default Header 
