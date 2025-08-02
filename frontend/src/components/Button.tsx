2e786262377673d9b9aca40193e450f9df1bca5e
GopiCharanReddy <gopicharanvgcr@gmail.com>
2025-08-03 00:06:22 +0530

    Update button and add brain and textfile

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -10,8 +10,8 @@ type ButtonProps = {
 }
 
 const variantStyles = {
-  "primary": "bg-slate-950 p-4 rounded-xl",
-  "secondary": "bg-purple-800 text-slate-950 font-semibold p-4 rounded-xl"
+  "primary": "bg-slate-300 text-slate-800 p-4 rounded-xl",
+  "secondary": "bg-purple-800 text-slate-950 text-white font-semibold p-4 rounded-xl"
 }
 const sizeStyles = {
   "sm": "p-2 py-3",
@@ -23,7 +23,7 @@ export const Button = (props: ButtonProps) => {
   return (
     <>
       <button className={`flex ${variantStyles[props.variant]} cursor-pointer m-2 ${sizeStyles[props.size]}`}  >
-        {props.startIcon ? <div className="pr-1">{props.startIcon}</div> : null}
+        {props.startIcon ? <div className="pr-2">{props.startIcon}</div> : null}
         {props.text}
         {props.endIcon ? <div className="pl-1">{props.endIcon}</div>: null}
       </button>
