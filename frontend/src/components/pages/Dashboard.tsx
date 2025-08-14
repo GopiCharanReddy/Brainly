7694c4f3d02ffadcd5e1e20b6afe53917d9d34aa
GopiCharanReddy <gopicharanvgcr@gmail.com>
2025-08-14 10:38:17 +0530

    Add Dashboard component with Card, Header, Sidebar, and CreateComponentModal

---
+++ frontend/src/components/pages/Dashboard.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import Card from '../Card'
+import CreateComponentModal from '..//CreateComponentModal'
+import Header from '../header/Header'
+import Sidebar from '../sidebar/Sidebar'
+
+function Dashboard() {
+  const [modelOpen, setModelOpen] = useState(true)
+  return (
+    <>
+      <div className='h-max min-h-screen flex bg-slate-100'>
+        <CreateComponentModal open={modelOpen} onClose={() => {setModelOpen(false)}} />
+        <Sidebar />
+        <div className='w-screen min-h-screen ml-72'>
+          <Header onClick={() => setModelOpen(true)} />
+          <div className='lg:flex'>
+          <Card type="youtube" link="https://www.youtube.com/watch?v=CAr02YlEJUc" title="DBMS Concepts" />
+          <Card type="youtube" link="https://www.youtube.com/watch?v=CAr02YlEJUc" title="DBMS Concepts" />
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Dashboard
